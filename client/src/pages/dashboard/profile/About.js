03e0041db6bb9acb64fe6f741b607de24f27c9a3
sevgi <sevgisarikaya05@gmail.com>
2023-02-03 18:31:56 +0100

    styled profile

--- client/src/pages/dashboard/profile/About.js
+++ client/src/pages/dashboard/profile/About.js
@@ -2,18 +2,38 @@ import { useContext } from "react";
 import Story from "../../../components/Story";
 import { ProfileContext } from "../../../context/profileContext";
 import StyledAbout from "./styles/About.styled";
+import { HiLocationMarker } from "react-icons/hi";
+import { MdSettings } from "react-icons/md";
 
 function About() {
   const { profileState } = useContext(ProfileContext);
   return (
     <StyledAbout>
       <section className="info">
-        <span>{profileState.profile.pronouns}</span>
-        <div>{profileState.profile.about}</div>
-        <span>{profileState.profile.location}</span>
-        <span>{profileState.profile.website}</span>
+        <div className="pronouns">{profileState.profile.pronouns}</div>
+        <div className="about">{profileState.profile.about}</div>
+        <div className="location">
+          <span className="icon">
+            <HiLocationMarker />
+          </span>
+          <span>{profileState.profile.location}</span>
+        </div>
+        <div>{profileState.profile.website}</div>
       </section>
       <section className="stories">
+        <header className="stories-header">
+          <h3>
+            <span>Stories by {profileState.profile.profileName}</span>
+            <span className="edit-icon">
+              <MdSettings />
+            </span>
+          </h3>
+
+          <div className="meta-data">
+            <span>0 Published Stories</span>
+            <span>3 Drafts (only visible to you)</span>
+          </div>
+        </header>
         {profileState.stories.map((story) => {
           return <Story key={story._id} story={story} />;
         })}
--- client/src/pages/dashboard/profile/styles/About.styled.js
+++ client/src/pages/dashboard/profile/styles/About.styled.js
@@ -1,20 +1,68 @@
 import styled from "styled-components";
 
 const StyledAbout = styled.div`
-  width: 80%;
-  margin: 0 auto;
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-gap: 30px;
+  padding: 0 2rem;
+  padding-bottom: 3rem;
+  display: flex;
+  justify-content: space-between;
+  @media only screen and (max-width: 1000px) {
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
+    .info {
+      min-width: 100%;
+      margin-bottom: 1rem;
+    }
+    .stories {
+      min-width: 100%;
+    }
+  }
 
   .info {
+    font-size: 15px;
+    color: #222;
+    width: 25%;
     padding: 2rem 1rem;
     height: fit-content;
     box-shadow: 0 1px 10px 0 rgb(34 34 34 / 8%), 0 4px 5px 0 rgb(34 34 34 / 10%);
+    .location {
+      margin: 1rem 0;
+      padding: 10px 0;
+      border-bottom: 1px solid #eee;
+      .icon {
+        margin-right: 10px;
+      }
+    }
   }
   .stories {
+    width: 65%;
     padding: 2rem 1rem;
     box-shadow: 0 1px 10px 0 rgb(34 34 34 / 8%), 0 4px 5px 0 rgb(34 34 34 / 10%);
+    .stories-header {
+      margin-bottom: 1.5rem;
+      h3 {
+        font-size: 24px;
+        font-weight: 600;
+        color: #222;
+        display: flex;
+        justify-content: space-between;
+        .edit-icon {
+          cursor: pointer;
+          font-size: 30px;
+          display: flex;
+          align-items: center;
+        }
+      }
+      .meta-data {
+        display: inline-block;
+        margin-top: 5px;
+        font-size: 15px;
+        color: #6f6f6f;
+        span {
+          margin-right: 10px;
+        }
+      }
+    }
   }
 `;
 
