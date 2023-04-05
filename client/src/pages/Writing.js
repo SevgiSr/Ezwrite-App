4369b2fe9cd3101d6ca34b6993c4d3b8a9145145
sevgi <sevgisarikaya05@gmail.com>
2023-04-05 22:45:59 +0300

    fixed backwards typing contentEditable div

--- client/src/pages/Writing.js
+++ client/src/pages/Writing.js
@@ -1,18 +1,39 @@
 import "../assets/Writing.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { handleKeyDown } from "../utils/handleKeyDown";
 import { Navbar } from "../components";
 import { useContext } from "react";
 import { MyStoryContext } from "../context/myStoryContext";
 import { useParams } from "react-router-dom";
+import he from "he";
 
 function Writing() {
-  const [chapter, setChapter] = useState({ body: "", title: "" });
+  const [chapterBody, setChapterBody] = useState("Type the text...");
+  const [chapterTitle, setChapterTitle] = useState("");
   const { storyState, editChapter, saveChapter } = useContext(MyStoryContext);
   const { story_id, chapter_id } = useParams();
+
+  const contentEditableRef = useRef(null);
+
+  useEffect(() => {
+    // Set the focus to the end of the contentEditable div
+    contentEditableRef.current.focus();
+    const range = document.createRange();
+    range.selectNodeContents(contentEditableRef.current);
+    range.collapse(false);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }, [chapterBody]);
+
   const handleChange = (e) => {
-    setChapter({ ...chapter, [e.target.name]: e.target.value });
+    setChapterTitle(e.target.value);
   };
+
+  const handleInput = (e) => {
+    setChapterBody(e.target.innerHTML);
+  };
+
   //as soon s u load, make get request to get chapther's title and content adn set it on reducer's chapter
   useEffect(() => {
     editChapter(story_id, chapter_id);
@@ -21,12 +42,19 @@ function Writing() {
   //after get request, chapter in reducer is set and setChapter sets title and content in frontend
   useEffect(() => {
     const { chapter } = storyState;
-    setChapter({ title: chapter.title, body: chapter.content });
+    console.log(chapter);
+    setChapterTitle(chapter.title);
+    setChapterBody(chapter.content);
   }, [storyState.chapter]);
 
   //saves in the backend and also cuz reducer's chapter changed useeffect gonna set it on frontend
   const handleSubmit = (e) => {
     e.preventDefault();
+    const content = document.getElementById("editStory").innerHTML;
+    const chapter = {
+      title: chapterTitle,
+      body: content,
+    };
     saveChapter(chapter, story_id, chapter_id);
   };
 
@@ -38,11 +66,11 @@ function Writing() {
           id="editTitle"
           name="title"
           onChange={handleChange}
-          value={chapter.title}
+          value={chapterTitle}
           placeholder="story title..."
         />
         <hr style={{ color: "gray", width: "60%" }} />
-        <textarea
+        {/* <textarea
           id="editStory"
           name="body"
           onChange={handleChange}
@@ -50,6 +78,17 @@ function Writing() {
           onKeyDown={handleKeyDown}
           style={{ height: "100vh" }}
           placeholder="Type your text..."
+        /> */}
+        <div
+          ref={contentEditableRef}
+          id="editStory"
+          contentEditable
+          onInput={handleInput}
+          onKeyDown={handleKeyDown}
+          style={{ minHeight: "100vh", outline: "none" }}
+          dangerouslySetInnerHTML={{
+            __html: he.decode(chapterBody ? chapterBody : ""),
+          }}
         />
       </div>
     </form>
